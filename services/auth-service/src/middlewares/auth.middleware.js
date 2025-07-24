05455ca37404f8a69191d49c1f893dff610d5a72
Skinjbir <mohammedredatarmidi@gmail.com>
2025-07-24 11:37:44 +0100

    test auth cicd

--- services/auth-service/src/middlewares/auth.middleware.js
+++ services/auth-service/src/middlewares/auth.middleware.js
@@ -26,7 +26,10 @@ module.exports = async (req, res, next) => {
     );
 
 
+
+
     
+
     if (result.rowCount === 0) {
       logger.warn('Access denied — token is not registered');
       return res.status(403).json({ error: 'Unrecognized token' });
